cee86d2af5269ac74d7b63c2e91bb0169953d5ae
emoren-dotcom <emre.oren@trendyol.com>
2025-07-27 18:56:06 +0300

    asd

--- commands.js
+++ commands.js
@@ -1,10 +1,9 @@
-// Office Add-in komutları
-function showTaskPane() {
-    Office.addin.showAsTaskpane();
-}
-
-// Office.js başlatma
-Office.onReady(() => {
-    console.log('Office Add-in hazır');
-});
-            
+function showPopup() {
+    Office.context.ui.displayDialogAsync('https://emoren-dotcom.github.io/trendyol-outlook-addon2/popup.html',
+      { height: 30, width: 20 },
+      function (result) {
+        console.log('Dialog opened');
+      }
+    );
+  }
+  
