72f6573b296846d1affe46377cff962b7a284977
emoren-dotcom <emre.oren@trendyol.com>
2025-07-27 19:37:10 +0300

    sss

--- commands.js
+++
@@ -1,9 +0,0 @@
-function showPopup() {
-    Office.context.ui.displayDialogAsync('https://emoren-dotcom.github.io/trendyol-outlook-addon2/popup.html',
-      { height: 30, width: 20 },
-      function (result) {
-        console.log('Dialog opened');
-      }
-    );
-  }
-  
